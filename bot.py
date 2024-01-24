2a26d52fab14f339ca74cce8de7b93d87c2b16d8
Yu <86376823+yuyu786@users.noreply.github.com>
2024-01-24 23:41:56 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,26 +89,26 @@ async def sendLogFile(c: Client, m: Message):
 async def loginHandler(c: Client, m: Message):
     user = UserSettings(m.from_user.id, m.from_user.first_name)
     if user.banned:
-        await m.reply_text(text=f"**Banned User Detected!**\n 🛡️ Unfortunately you can't use me\n\nContact: 🈲 @{Config.OWNER_USERNAME}", quote=True)
+        await m.reply_text(text=f"**Banned User Detected!\nUnfortunately You Can't Use Me\n\nContact: @{Config.OWNER_USERNAME}**", quote=True)
         return
     if user.user_id == int(Config.OWNER):
         user.allowed = True
     if user.allowed:
-        await m.reply_text(text=f"**Dont Spam**\n ⚡You can use me!!\n Type /about For Bot Features\n Type /help For How To use This Bot!", quote=True)
+        await m.reply_text(text=f"**Dont Spam\nYou Can Use Me!!\nType /about For Bot Features\nType /help For How To Use This Bot!**", quote=True)
     else:
         try:
             passwd = m.text.split(" ", 1)[1]
         except:
-            await m.reply_text("**Command:**\n `/login <password>`\n\n**Usage:**\n Get the password from here https://t.me/SourcePleaseOfficial/19",quote=True,disable_web_page_preview=True,parse_mode=enums.parse_mode.ParseMode.MARKDOWN)
+            await m.reply_text("**Command: `/login <password>`\n\nGet The Password From Here https://t.me/SourcePleaseOfficial/19**",quote=True,disable_web_page_preview=True,parse_mode=enums.parse_mode.ParseMode.MARKDOWN)
         passwd = passwd.strip()
         if passwd == Config.PASSWORD:
             user.allowed = True
             await m.reply_text(
-                text=f"**Login passed ✅,**\n ⚡ Now you can use me!!", quote=True
+                text=f"**Login passed ✅,**\nNow You Can Use Me!!", quote=True
             )
         else:
             await m.reply_text(
-                text=f"**Login failed ❌,**\n 🛡️ Unfortunately you can't use me\n\nContact: 🈲 @{Config.OWNER_USERNAME}",
+                text=f"**Login failed ❌,**\nUnfortunately You Can't Use Me\n\nContact: @{Config.OWNER_USERNAME}",
                 quote=True,
             )
     user.set()
@@ -196,7 +196,7 @@ async def start_handler(c: Client, m: Message):
     if m.from_user.id != int(Config.OWNER):
         if user.allowed is False:
             res = await m.reply_text(
-                text=f"Hi **{m.from_user.first_name}**\n\n 🛡️ Unfortunately you can't use me\n\n** `/login <password>`\n\n**Usage:**\n Get the password from here https://t.me/SourcePleaseOfficial/19 ** ",
+                text=f"Hi**{m.from_user.first_name}**\n\n**Unfortunately You Can't Use Me\n\n`/login <password>`\n\nGet The Password From Here https://t.me/SourcePleaseOfficial/19**",
                 quote=True,
 		disable_web_page_preview=True,
             )
@@ -205,7 +205,7 @@ async def start_handler(c: Client, m: Message):
         user.allowed = True
         user.set()
     res = await m.reply_text(
-        text=f"Hi **{m.from_user.first_name}**\n\n ⚡I am a File/Video Merger Bot\n\n 😎I Can Merge Telegram Files!, Also Subtitles/Audio Extract From Video File And Upload it to Telegram\n\n**Owner: 🈲 @{Config.OWNER_USERNAME}** ",
+        text=f"Hi **{m.from_user.first_name}**\n\n**I Am A File/Video Merger Bot\n\nI Can Merge Telegram Files!, Also Subtitles/Audio Extract From Video File And Upload It To Telegram\n\n**Owner: @{Config.OWNER_USERNAME}**",
         quote=True,
     )
     del user
@@ -220,7 +220,7 @@ async def files_handler(c: Client, m: Message):
     if user_id != int(Config.OWNER):
         if user.allowed is False:
             res = await m.reply_text(
-                text=f"Hi **{m.from_user.first_name}**\n\n 🛡️ Unfortunately you can't use me\n\n**Contact: 🈲 @{Config.OWNER_USERNAME}** ",
+                text=f"Hi**{m.from_user.first_name}**\n\n**Unfortunately You Can't Use Me\n\nContact: @{Config.OWNER_USERNAME}**",
                 quote=True,
             )
             return
@@ -400,7 +400,7 @@ async def photo_handler(c: Client, m: Message):
     # if m.from_user.id != int(Config.OWNER):
     if not user.allowed:
         res = await m.reply_text(
-            text=f"Hi **{m.from_user.first_name}**\n\n 🛡️ Unfortunately you can't use me\n\n**Contact: 🈲 @{Config.OWNER_USERNAME}** ",
+            text=f"Hi**{m.from_user.first_name}**\n\n**Unfortunately You Can't Use Me\n\nContact: @{Config.OWNER_USERNAME}**",
             quote=True,
         )
         del user
