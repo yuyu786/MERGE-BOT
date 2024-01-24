2c08f4da077ae02259031acdfc5b58580fb90ff0
Yu <86376823+yuyu786@users.noreply.github.com>
2024-01-24 23:24:54 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ if os.path.exists("downloads") == False:
     os.makedirs("downloads")
 
 
-@mergeApp.on_message(filters.command(["log"]) & filters.user(Config.OWNER_USERNAME))
+@mergeApp.on_message(filters.command(["log"]) & filters.user(Config.OWNER))
 async def sendLogFile(c: Client, m: Message):
     await m.reply_document(document="./mergebotlog.txt")
     return
@@ -147,7 +147,7 @@ async def stats_handler(c: Client, m: Message):
 @mergeApp.on_message(
     filters.command(["broadcast"])
     & filters.private
-    & filters.user(Config.OWNER_USERNAME)
+    & filters.user(Config.OWNER)
 )
 async def broadcast_handler(c: Client, m: Message):
     msg = m.reply_to_message
